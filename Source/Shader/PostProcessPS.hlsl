48670fe23c95ed5a1a1c3c338ee7824bfcbf52d3
mame0622 <mame.like.ww@gmail.com>
2024-01-11 21:25:46 +0900

    ビネット実装

--- Source/Shader/PostProcessPS.hlsl
+++ Source/Shader/PostProcessPS.hlsl
@@ -3,6 +3,14 @@
 SamplerState samplerState : register(s0);
 Texture2D textureMap : register(t0);
 float4 main(PSIn psIn) : SV_TARGET
-{    
-    return textureMap.Sample(samplerState, psIn.texcoord);
+{   
+    float4 color = textureMap.Sample(samplerState, psIn.texcoord);
+    
+    float2 samplePoint = psIn.texcoord;
+    float vignette = length(float2(0.5, 0.5) - psIn.texcoord);
+    //vignette = clamp(vignette - vignetteValue, 0, 1);
+    vignette = clamp(vignette - 0.2, 0, 1);
+    color.rgb -= vignette;
+    
+    return color;
 }
